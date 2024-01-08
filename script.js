bd945d52526fabddfb5175f04f3e70610a0aae61
Sandro Gakharia <sandrogakharia@gmail.com>
2024-01-08 06:20:02 +0400

    Add a calculator skeleton without functionality

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, op, b) {
+  switch (op) {
+    case '+':
+      add(a, b);
+      break;
+    case '-':
+      subtract(a, b);
+      break;
+    case '*':
+      multiply(a, b);
+      break;
+    case '/':
+      divide(a, b);
+      break;
+  }
+}
+
+let argOne;
+let op;
+let argTwo;
